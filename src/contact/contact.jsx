a1510d5997ac4ae3f4bcecc6ec7d69c448d8920f
Gabriel Feipe <gabrielferreiro963@gmail.com>
2024-07-29 18:32:23 -0300

    all sounds added

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -1,14 +1,26 @@
 import './contact.css'
+import {useRef} from 'react'
 import gitHub from "../imgs/contatos/github.png"
 import gMail from "../imgs/contatos/gmail.png"
 import link from "../imgs/contatos/link-quebrado.png"
 import copy from "../imgs/contatos/copia-de.png"
+import sound from '../sounds/hover.wav'
 
 function contact() {
+  const audio = useRef()
+
+  function play_audio(){
+    audio.current.currentTime = "0"
+    audio.current.volume = '.3'
+    audio.current.play()
+  }
+
   return (
     <section id="contact">
+      <audio src={sound} ref={audio}></audio>
         <h2>Contatos</h2>
         <div 
+            onMouseEnter={() => play_audio()}
             className='gitHub' 
             onClick={() => window.open("https://github.com/FerreiroINTZ")}>
             <img src={gitHub} alt="gitHub" className='logos'/>
@@ -16,6 +28,7 @@ function contact() {
             <img src={link} alt="link" className='actions' />
         </div>
         <div 
+            onMouseEnter={() => play_audio()}
             className='e-mail'
             onClick={() => navigator.clipboard.write('gabrielferreiro963@gmail.com')}>
             <img src={gMail} alt="e-mail" className='logos' />
--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./projects.css"
-import eu from "../imgs/projects/gb7.jpg"
+import sound from '../sounds/projects-hover.mp3'
 
 function projects() {
+  const audio = useRef()
+  
+  function play_audio(){
+    audio.current.currentTime = '0'
+    audio.current.volume = '.3'
+    audio.current.play()
+  }
+
   return (
     <section id='projects-section'>
+      <audio src={sound} ref={audio}></audio>
         <h2>Projetos</h2>
         <ul>
-          <li>
+          <li onMouseEnter={() => play_audio()}>
             <div className="img"><img src="https://www.creativefabrica.com/wp-content/uploads/2019/10/01/Abstract-Technology-Gear-Background-by-ojosujono96.jpg" alt="" /></div>
             <button>Jogar</button>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolorem deserunt, ad autem dicta expedita deleniti. Consectetur sapiente esse tempora temporibus laudantium ut ex quasi cupiditate, dicta voluptatibus voluptatem quod?</p>
           </li>
-          <li>
+          <li  onMouseEnter={() => play_audio()}>
             <div className="img"><img src="https://www.creativefabrica.com/wp-content/uploads/2019/10/01/Abstract-Technology-Gear-Background-by-ojosujono96.jpg" alt="" /></div>
             <button>Jogar</button>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolorem deserunt, ad autem dicta expedita deleniti. Consectetur sapiente esse tempora temporibus laudantium ut ex quasi cupiditate, dicta voluptatibus voluptatem quod?</p>
